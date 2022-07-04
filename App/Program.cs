5e4cdd6cffdacf6169898ac95a5e85107a239a62
esuard <erwannsuardpro@gmail.com>
2022-07-04 14:09:58 +0200

    Premier commit

---
+++ App/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+
+namespace App;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        
+        
+        ConsoleKeyInfo key;
+        bool ok = false;
+        while(!ok)
+        {
+            Write("Appuyez sur entrée pour clear la console : ");
+            key = ReadKey();
+            ok = key.Key switch
+            {
+                ConsoleKey.Enter => true,
+                _ => false
+                
+            };
+            if(key.Key == ConsoleKey.Enter)
+            {
+                ok = true;
+            }
+            else
+            {
+                WriteLine($"\nVous avez appuyé sur {key.Key}. Mauvaise touche.");
+            }
+
+        }
+
+        WriteLine($"\nBravo\n");
+        
+        Clear();
+
+
+    }
+}
